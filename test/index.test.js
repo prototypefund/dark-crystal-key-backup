3fe7fa88ad6b3afe9382506edc5899a26909e432
ameba23 <ameba23@systemli.org>
2020-01-28 17:33:19 +0100

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -1,23 +1,22 @@
 const { describe } = require('tape-plus')
 const keyBackup = require('..')
 const s = require('key-backup-crypto')
-
-const custodians = []
-for (let i = 0; i < 5; i++) {
-  custodians.push(s.encryptionKeypair())
-}
-
-const sObj = {
-  secret: s.randomBytes(32),
-  label: 'My private key',
-  shards: 5,
-  quorum: 3,
-  signingKeypair: s.signingKeypair(),
-  custodians: custodians.map(c => c.publicKey)
-}
+const pull = require('pull-stream')
 
 describe('share', (context) => {
   context('basic', (assert, next) => {
+    const custodians = []
+    for (let i = 0; i < 5; i++) {
+      custodians.push(s.encryptionKeypair())
+    }
+
+    const sObj = {
+      secret: s.randomBytes(32),
+      label: 'My private key',
+      shards: 5,
+      quorum: 3,
+      custodians: custodians.map(c => c.publicKey)
+    }
     const member = keyBackup()
     assert.ok(member.keypair, 'has keypair')
     member.share(sObj).then((boxedMessages) => {
@@ -27,5 +26,37 @@ describe('share', (context) => {
       console.log(err)
       next()
     })
+  })
+
+  context('multiple actors', (assert, next) => {
+    const feeds = {}
+    const custodians = []
+    const numMembers = 5
+    for (let i = 0; i < numMembers; i++) {
+      custodians.push(keyBackup())
+    }
+    const secretOwner = keyBackup()
+    const sObj = {
+      secret: s.randomBytes(32),
+      label: 'My private key',
+      shards: 5,
+      quorum: 3,
+      custodians: custodians.map(c => c.encryptionKeypair.publicKey),
+      publish (message) {
+        messages.push(message)
+      },
+      query () {
+        return pull(pull.values(messages))
+      }
+    }
+    secretOwner.share(sObj).then((boxedMessages) => {
+      assert.equal(boxedMessages.length, 6, 'Gives expected number of messages')
+        
+      next()
+    }).catch((err) => {
+      console.log(err)
+      next()
+    })
+    next()
   })
 })
