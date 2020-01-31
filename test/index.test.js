c34999af4591ca7dd24275b5adaa381b144d7f6b
ameba23 <ameba23@systemli.org>
2020-01-31 10:06:54 +0100

    test with multiple actors

--- test/index.test.js
+++ test/index.test.js
@@ -4,10 +4,23 @@ const s = require('key-backup-crypto')
 const pull = require('pull-stream')
 
 describe('share', (context) => {
+  let messages
+  function publish (message) {
+    messages.push(message)
+  }
+
+  function query () {
+    return pull(pull.values(messages))
+  }
+
+  context.beforeEach(() => {
+    messages = []
+  })
+
   context('basic', (assert, next) => {
     const custodians = []
     for (let i = 0; i < 5; i++) {
-      custodians.push(s.encryptionKeypair())
+      custodians.push(s.keypair())
     }
 
     const sObj = {
@@ -17,46 +30,63 @@ describe('share', (context) => {
       quorum: 3,
       custodians: custodians.map(c => c.publicKey)
     }
-    const member = keyBackup()
+    const member = keyBackup({ publish, query })
     assert.ok(member.keypair, 'has keypair')
-    member.share(sObj).then((boxedMessages) => {
-      assert.equal(boxedMessages.length, 6, 'Gives expected number of messages')
+    member.share(sObj).then((root) => {
+      assert.ok(root, 'gives a root id')
       next()
     }).catch((err) => {
-      console.log(err)
+      assert.error(err)
       next()
     })
   })
 
   context('multiple actors', (assert, next) => {
-    const feeds = {}
     const custodians = []
     const numMembers = 5
     for (let i = 0; i < numMembers; i++) {
-      custodians.push(keyBackup())
+      custodians.push(keyBackup({ publish, query }))
     }
-    const secretOwner = keyBackup()
+    const secretOwner = keyBackup({ publish, query })
     const sObj = {
       secret: s.randomBytes(32),
       label: 'My private key',
       shards: 5,
       quorum: 3,
-      custodians: custodians.map(c => c.encryptionKeypair.publicKey),
-      publish (message) {
-        messages.push(message)
-      },
-      query () {
-        return pull(pull.values(messages))
-      }
+      custodians: custodians.map(c => c.keypair.publicKey)
     }
-    secretOwner.share(sObj).then((boxedMessages) => {
-      assert.equal(boxedMessages.length, 6, 'Gives expected number of messages')
-        
-      next()
+    secretOwner.share(sObj).then((root) => {
+      assert.ok(root, 'Gives root ID')
+      secretOwner.request(root, null, (err, numRequests) => {
+        assert.error(err, 'No error on request')
+        assert.equal(numRequests, 5, 'Expected number of requests published')
+        makeReplies(root)
+      })
     }).catch((err) => {
-      console.log(err)
+      assert.error(err, 'No error on share')
       next()
     })
-    next()
+
+    function makeReplies (root) {
+      pull(
+        pull.values(custodians),
+        pull.asyncMap((custodian, cb) => {
+          custodian.reply((err, numReplies) => {
+            assert.error(err, 'No error on reply')
+            assert.equal(numReplies, 1, 'Single reply published')
+            cb(null, numReplies)
+          })
+        }),
+        pull.collect((err) => {
+          assert.error(err, 'No error')
+          secretOwner.combine(root, (err, secretObj) => {
+            assert.error(err, 'No error on combine')
+            assert.equal(secretObj.label, sObj.label, 'Label correctly retrieved')
+            assert.equal(secretObj.secret.toString('hex'), sObj.secret.toString('hex'), 'Successfully recovered secret')
+            next()
+          })
+        })
+      )
+    }
   })
 })
